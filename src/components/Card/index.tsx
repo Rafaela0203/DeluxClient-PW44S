201e9246207a59730b5e3a06f8958a0cfa9bcd3f
Dante-doo <anapaulavolpatto2004@gmail.com>
2025-02-11 19:59:32 -0300

    Product
    Criação da ProductPage

--- src/components/Card/index.tsx
+++ src/components/Card/index.tsx
@@ -7,28 +7,32 @@ import {
     Text,
     CardFooter,
 } from '@chakra-ui/react';
-import {IProduct} from "@/commons/interfaces.ts";
-import {ButtonAddToCart} from "@/components/ButtonAddToCart";
+import { IProduct } from "@/commons/interfaces.ts";
+import { ButtonAddToCart } from "@/components/ButtonAddToCart";
+import { Link } from "react-router-dom";
 
-export function Cards({product}: { product: IProduct }) {
+export function Cards({ product }: { product: IProduct }) {
     const formatPrice = (price: number) => price.toFixed(2).replace(".", ",");
 
     return (
-        <>
-            <Card  >
+        <Link to={`/product/${product.id}`} style={{ textDecoration: 'none' }}>
+            <Card _hover={{ boxShadow: "lg", transform: "scale(1.02)", transition: "0.2s" }}>
                 <CardBody>
                     <Image
                         src={product.image}
                         alt={product.name}
-                        borderRadius='lg'
+                        borderRadius="lg"
+                        objectFit="cover"
+                        w="100%"
+                        h="200px"
                     />
                     <Stack mt='6' spacing='2'>
-                        <Heading size='md' color={"red.600"}>{product.brand}</Heading>
+                        <Heading size='md' color="red.600">{product.brand}</Heading>
                         <Text>
-                            {product.name} {/* Aqui você pode usar a descrição do produto */}
+                            {product.name}
                         </Text>
                         <Text color='red.600' fontSize='2xl'>
-                            R${formatPrice(product.price)}
+                            R$ {formatPrice(product.price)}
                         </Text>
                     </Stack>
                 </CardBody>
@@ -36,6 +40,6 @@ export function Cards({product}: { product: IProduct }) {
                     <ButtonAddToCart product={product} />
                 </CardFooter>
             </Card>
-        </>
+        </Link>
     );
 }
